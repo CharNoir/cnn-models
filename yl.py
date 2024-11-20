347e78c148f6edde477d1cfebca95b2663a60edb
CharNoir <59160057+CharNoir@users.noreply.github.com>
2024-11-20 21:25:27 +0100

    Update yl.py

--- yl.py
+++ yl.py
@@ -5,5 +5,7 @@ edgetpu_model = YOLO("yolov8n_full_integer_quant_edgetpu.tflite")
 
 # Run inference
 results = edgetpu_model("https://ultralytics.com/images/bus.jpg")
+print(results)
 
+results = edgetpu_model("https://ultralytics.com/images/bus.jpg")
 print(results)
