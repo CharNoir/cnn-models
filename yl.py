6cce78ba41fde26d30453f3ac843c8ba79548882
CharNoir <59160057+CharNoir@users.noreply.github.com>
2024-11-20 21:23:23 +0100

    Create yl.py

---
+++ yl.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+# Load the exported TFLite Edge TPU model
+edgetpu_model = YOLO("yolov8n_full_integer_quant_edgetpu.tflite")
+
+# Run inference
+results = edgetpu_model("https://ultralytics.com/images/bus.jpg")
+
+print(results)
