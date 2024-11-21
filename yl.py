dce1094c61a844b076dafb026496696a4b3e1f10
CharNoir <59160057+CharNoir@users.noreply.github.com>
2024-11-21 16:46:00 +0100

    .

--- yl.py
+++ yl.py
@@ -7,5 +7,7 @@ edgetpu_model = YOLO("yolov8n_full_integer_quant_edgetpu.tflite")
 results = edgetpu_model("bus.jpg", imgsz=640)
 print(results)
 
-results = edgetpu_model("bus.jpg", imgsz=224)
+
+edgetpu_model = YOLO("yolov8n_320_full_integer_quant_edgetpu.tflite")
+results = edgetpu_model("bus.jpg", imgsz=320)
 print(results)
