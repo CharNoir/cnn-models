bb24b513b5e98e73dd9f172288cd5fb562edbfc5
CharNoir <59160057+CharNoir@users.noreply.github.com>
2024-11-21 16:55:06 +0100

    Update bench.py

--- bench.py
+++ bench.py
@@ -1,4 +1,4 @@
 from ultralytics.utils.benchmarks import benchmark
 
 # Benchmark on GPU
-benchmark(model="yolo11n.pt", data="coco8.yaml", imgsz=320, int8=True)
+benchmark(model="yolov8n.pt", data="coco8.yaml", imgsz=320, int8=True)
