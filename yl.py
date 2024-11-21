904dc086c5cf6883ec300e7d597f735136c19ec5
CharNoir <59160057+CharNoir@users.noreply.github.com>
2024-11-21 16:09:48 +0100

    .

--- yl.py
+++ yl.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 
 # Load the exported TFLite Edge TPU model
-edgetpu_model = YOLO("yolov8n_full_integer_quant_edgetpu.tflite")
+edgetpu_model = YOLO("yolov5s-int8-224_edgetpu.tflite")
 
 # Run inference
 results = edgetpu_model("bus.jpg", imgsz=640)
 print(results)
 
-results = edgetpu_model("bus.jpg", imgsz=640)
+results = edgetpu_model("bus.jpg", imgsz=224)
 print(results)
