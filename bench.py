1ed6c5a7700cd78255b8258bc9e675f2a8fbe568
CharNoir <59160057+CharNoir@users.noreply.github.com>
2024-11-26 18:23:08 +0100

    deleted smth

--- bench.py
+++ bench.py
@@ -1,4 +1,30 @@
-from ultralytics.utils.benchmarks import benchmark
+from ultralytics import YOLO
 
-# Benchmark on GPU
-benchmark(model="yolov8n.pt", data="coco8.yaml", imgsz=540)
+#benchmark(model="yolov8n.pt", data="coco8.yaml")
+
+#benchmark(model="yolov8n.pt", data="coco.yaml")
+
+def validate_model(model_path, dataset_path):
+     # Load the YOLO model
+    model = YOLO(model_path, task='detect')
+
+        # Run validation
+    metrics = model.val(data=dataset_path, imgsz=640)
+
+        # Format results
+    results = (
+        f"Model: {model_path}\n"
+        f"Dataset: {dataset_path}\n"
+        f"mAP50-95: {metrics.box.map:.4f}\n"
+        f"F1 score: {(2*metrics.box.mp*metrics.box.mr / (metrics.box.mp+metrics.box.mr)):.4f}\n"
+        f"Mean Precision: {metrics.box.mp:.4f}\n"
+        f"Mean Recall: {metrics.box.mr:.4f}\n"
+        f"Speed (Inference): {metrics.speed['inference']:.2f} ms/image\n"
+        "---------------------------------------------\n"
+    )
+
+        # Print results to console
+    print(results)
+
+        
+validate_model("yolov8n.pt", "coco8.yaml")
